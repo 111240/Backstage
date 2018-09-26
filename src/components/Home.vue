58ec0281a62193ffb949a37c9fe487c5748c774c
chenyi <1240513273@qq.com>
2018-09-26 23:42:58 +0800

    第一次推送

---
+++ src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <Header/>
+    <h1>Home</h1>
+    <!-- <router-view/> -->
+  </div>
+</template>
+
+<script>
+import Header from './header'
+export default {
+  name: 'Home',
+  data () {
+    return {
+
+    }
+  },
+  components: {
+    Header
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/header/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="header">
+    <div class="top">
+
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Head',
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+<style lang="scss">
+.header{
+  width: 100%;
+  .top{
+    height: 100px;
+    width: 100%;
+    background-color: greenyellow;
+  }
+}
+</style>
