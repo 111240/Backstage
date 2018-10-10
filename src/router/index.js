5408919af3f2ff553d7222249cc6a9426664d17a
chenyi <1240513273@qq.com>
2018-10-10 11:47:16 +0800

    路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/components/Home'
-import SonHome from '@/components/effectComponent/home'
-import Login from '@/components/effectComponent/login'
+// import Home from '@/components/Home'
+// import SonHome from '@/components/effectComponent/home'
+// import Login from '@/components/effectComponent/login'
 // import userList from '@/components/effectComponent/userList'
-import userInfo from '@/components/effectComponent/userList/userInfo'
+// import userInfo from '@/components/effectComponent/userList/userInfo'
 Vue.use(Router)
 
 const router = new Router({
@@ -12,22 +12,22 @@ const router = new Router({
     {
       path: '/',
       name: 'Home',
-      component: Home,
+      component: resolve => require(['@/components/Home'], resolve),
       children: [
         {
           path: '/sonhome',
           name: '首页',
-          component: SonHome
+          component: resolve => require(['@/components/effectComponent/home'], resolve)
         },
         {
           path: '/login',
           name: '登陆',
-          component: Login
+          component: resolve => require(['@/components/effectComponent/login'], resolve)
         },
         {
           path: '/userInfo',
           name: '用户信息',
-          component: userInfo
+          component: resolve => require(['@/components/effectComponent/userList/userInfo'], resolve)
         }
       ],
       redirect: to => {
